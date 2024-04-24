662a6b011ab84df8438f3666c7b4f51065137137
alifertah <alifertah42@gmail.com>
2024-04-24 09:23:22 +0100

    fix

--- rideshare_frontend/src/components/layout/header.jsx
+++ rideshare_frontend/src/components/layout/header.jsx
@@ -1,25 +1,23 @@
-import axios from 'axios';
-import Image from 'next/image'
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 function Header() {
+  const [isDropDown, setIsDropDown] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const [isDropDown, setIsDropDown] = useState(false)
-  
-  const handleDropDownToggle = () =>{
+  const handleDropDownToggle = () => {
     setIsDropDown(!isDropDown);
-  }
-  
-  useEffect(() => {
-    const authToken = () => {
-      const tokenCookie = document.cookie.split('; ').find(row => row.startsWith('token='));
-      return tokenCookie ? tokenCookie.split('=')[1] : null;
-    };
+  };
 
-    axios.defaults.headers.common['Authorization'] = authToken() ? `Bearer ${authToken()}` : '';
+  useEffect(() => {
+    const authToken = document.cookie.split('; ').find(row => row.startsWith('token='));
+    setIsLoggedIn(!!authToken);
+
+    axios.defaults.headers.common['Authorization'] = authToken ? `Bearer ${authToken.split('=')[1]}` : '';
   }, []);
-  
+
   const logout = () => {
     axios.post("http://127.0.0.1:8000/logout")
       .then(response => {
@@ -28,56 +26,49 @@ function Header() {
           const cookieName = cookie.split("=")[0].trim();
           document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
         });
+        setIsLoggedIn(false);
       })
       .catch(error => {
-        console.error("Error :: ", error);
+        console.error("Error:", error);
       });
   };
 
-  const [cookies, setCookies] = useState("")
-  useEffect (()=>{
-    setCookies(document.cookie)
-  }, [])
   return (
-    <div className='bg-[#161616] text-[#F8F8F8] h-16 flex w-full justify-around items-center'>
+    <div className="bg-[#161616] text-[#F8F8F8] h-16 flex justify-between items-center px-4 sm:px-6 lg:px-8">
+      <div>
         <Image src="/imgs/logo.png" width={60} height={100} />
-
-        {
-          cookies ? (
-          <nav className='flex items-center'>
-              <Link href='/newTrip' className='flex items-center rounded px-4 py-2 ml-4 cursor-pointer'><Image className='mr-4 cursor-pointer' src="/icons/New.svg" height={20} width={20} /><span>New</span></Link>
-              <Link href='/availableTrips' className='flex items-center rounded px-4 py-2 ml-4 cursor-pointer'><span>Availabale trips</span></Link>
-              <div className='flex items-center  rounded px-4 py-2 ml-4 cursor-pointer'><Image className='mr-4 cursor-pointer' src="/icons/search.svg" height={20} width={20} /><span>Search</span></div>
-            <div className='relative'>
-              <div className='flex items-center bg-[#2D2D2D] rounded px-8 py-2 ml-4 cursor-pointer'><span>John Doe</span>
-              <Image className={`ml-4 transition-transform transform ${isDropDown ? 'rotate-180' : ''}`}
+      </div>
+      <nav className="flex items-center">
+        {isLoggedIn ? (
+          <>
+            <Link href="/newTrip" className="flex items-center rounded px-4 py-2 ml-4 cursor-pointer"><Image className="mr-2" src="/icons/New.svg" height={20} width={20} /><span className="hidden sm:inline-block">New</span></Link>
+            <Link href="/availableTrips" className="flex items-center rounded px-4 py-2 ml-4 cursor-pointer"><span className="hidden sm:inline-block">Available trips</span></Link>
+            <div className="flex items-center bg-[#2D2D2D] rounded px-4 py-2 ml-4 cursor-pointer relative">
+              <span className="hidden sm:inline-block">John Doe</span>
+              <Image
+                className={`ml-2 transition-transform transform ${isDropDown ? 'rotate-180' : ''}`}
                 onClick={handleDropDownToggle}
-                src="/icons/dropdown.svg" height={10} width={10} />
-              </div>
+                src="/icons/dropdown.svg"
+                height={10}
+                width={10}
+              />
               {isDropDown && (
-                <div className='absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10'>
-              <div className='py-1'>
-                <Link href="/dashboard" className='block px-4 py-2 text-gray-800 hover:bg-gray-200'>Dashboard</Link>
-                <a href="#" className='block px-4 py-2 text-gray-800 hover:bg-gray-200'>Settings</a>
-                <div onClick={logout} className='cursor-pointer block px-4 py-2 text-gray-800 hover:bg-gray-200'>Logout</div>
-                <Link href="/register" className='block px-4 py-2 text-gray-800 hover:bg-gray-200'>Register</Link>
-              </div>
+                <div className="absolute right-0 mt-56 w-48 bg-white rounded-md shadow-lg z-10">
+                  <div className="py-1">
+                    <Link href="/dashboard" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Dashboard</Link>
+                    <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Settings</a>
+                    <div onClick={logout} className="cursor-pointer block px-4 py-2 text-gray-800 hover:bg-gray-200">Logout</div>
+                  </div>
+                </div>
+              )}
             </div>
-          )}
-          </div>
-        </nav>
-          ) : (
-
-          <nav className='flex items-center'>
-            <a
-            href='/login'
-            className='flex items-center bg-[#2D2D2D] rounded px-8 py-2 ml-4 cursor-pointer'><span>Login</span>
-          </a>
-        </nav>
-          )
-        }
+          </>
+        ) : (
+          <a href="/login" className="flex items-center bg-[#2D2D2D] rounded px-4 py-2 ml-4 cursor-pointer">Login</a>
+        )}
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- rideshare_frontend/src/pages/posts/[id].js
+++ rideshare_frontend/src/pages/posts/[id].js
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { toast, ToastContainer } from 'react-toastify';
 
-export function Post () {
+export function Post() {
   const router = useRouter();
   const { id } = router.query;
   const [post, setPost] = useState(null);
@@ -18,7 +18,7 @@ export function Post () {
 
         const response = await axios.get(`http://127.0.0.1:8000/trip/${id}`, { headers });
         setPost(response.data);
-       
+
       } catch (error) {
         console.error('Error fetching post:', error);
       }
@@ -29,91 +29,90 @@ export function Post () {
     }
   }, [id]);
 
-  // if (!post) {
-  //   return <div>Loading...</div>;
-  // }
   const authTokenn = () => {
     const tokenCookie = document.cookie.split('; ').find(row => row.startsWith('token='));
     return tokenCookie ? tokenCookie.split('=')[1] : null;
   };
 
-  const loggedInUserId = () => {
-    const tokenCookie = document.cookie.split('; ').find(row => row.startsWith('userId='));
-    return tokenCookie ? tokenCookie.split('=')[1] : null;
-  };
+  const handleBook = async (postOwner) => {
+    axios.defaults.headers.common['Authorization'] = authTokenn() ? `Bearer ${authTokenn()}` : '';
+    try {
+      const response = await axios.post(`http://127.0.0.1:8000/book/${id}`, { "userId": postOwner });
 
-
-const handleBook = async(postOwner) =>{
-  axios.defaults.headers.common['Authorization'] = authTokenn() ? `Bearer ${authTokenn()}` : '';
-  try {
-    const response = await axios.post(`http://127.0.0.1:8000/book/${id}`, {"userId" : postOwner});
-    
-    if (response.status === 200) {
-      toast.success('Booking successful!', {
-        position: 'top-right',
-        autoClose: 3000, 
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-    } else {
-      toast.error('Booking failed. Please try again.');
+      if (response.status === 200) {
+        toast.success('Booking successful!', {
+          position: 'top-right',
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      } else {
+        toast.error('Booking failed. Please try again.');
+      }
+    } catch (error) {
+      console.error('Error booking:', error);
+      toast.error('An error occurred while booking. Please try again later.');
     }
-  } catch (error) {
-    console.error('Error booking:', error);
-    toast.error('An error occurred while booking. Please try again later.');
   }
-}
+
   return (
     <>
-      {post ? (<div class=" w-[50%] container mx-auto p-6 bg-gray-100 shadow-lg rounded-lg space-y-8 flex flex-col items-center">
-      <ToastContainer /> 
-  <div class="text-2xl font-bold mb-4">{post.date}</div>
-  <div class="w-full">
-    <div class="bg-white p-4 rounded-lg shadow-md">
-      <div class="text-xl font-bold">Departure</div>
-      <div class="mt-2">{post.departure}</div>
-    </div>
-  </div>
-  <div class="w-full">
-    <div class="bg-white p-4 rounded-lg shadow-md">
-      <div class="text-xl font-bold">Arrival</div>
-      <div class="mt-2">{post.arrival}</div>
-    </div>
-  </div>
-  <div class="w-full">
-    <div class="bg-white p-4 rounded-lg shadow-md">
-      <div class="text-xl font-bold">Price per passenger</div>
-      <div class="mt-2">{post.price}.00$</div>
-    </div>
-  </div>
-  <div class="w-full">
-    <div class="bg-white p-4 rounded-lg shadow-md">
-      <div class="text-xl font-bold">Car</div>
-      <div class="mt-2">{post.car}</div>
-    </div>
-  </div>
-  <div class="w-full">
-    <div class="bg-white p-4 rounded-lg shadow-md">
-      <div class="text-xl font-bold">Description</div>
-      <div class="mt-2">Julie</div>
-    </div>
-  </div>
-  <div class="w-full">
-    <div class="bg-white p-4 rounded-lg shadow-md flex items-center justify-center cursor-pointer" onClick={() => handleBook(post.owner)}>
-      <Primary text="BOOK NOW"/>
-    </div>
-  </div>
-</div>
+      {post ? (
+        <div className="container mx-auto p-6 bg-gray-100 shadow-lg rounded-lg space-y-8 flex flex-col items-center">
 
-)
-       :
-       ("Loading ...")
-    }
-  </>
-    
+          <ToastContainer />
+
+          <div className="text-2xl font-bold mb-4">{post.date}</div>
+
+          <div className="w-full">
+            <div className="bg-white p-4 rounded-lg shadow-md">
+              <div className="text-xl font-bold">Departure</div>
+              <div className="mt-2">{post.departure}</div>
+            </div>
+          </div>
+
+          <div className="w-full">
+            <div className="bg-white p-4 rounded-lg shadow-md">
+              <div className="text-xl font-bold">Arrival</div>
+              <div className="mt-2">{post.arrival}</div>
+            </div>
+          </div>
+
+          <div className="w-full">
+            <div className="bg-white p-4 rounded-lg shadow-md">
+              <div className="text-xl font-bold">Price per passenger</div>
+              <div className="mt-2">{post.price}.00$</div>
+            </div>
+          </div>
+
+          <div className="w-full">
+            <div className="bg-white p-4 rounded-lg shadow-md">
+              <div className="text-xl font-bold">Car</div>
+              <div className="mt-2">{post.car}</div>
+            </div>
+          </div>
+
+          <div className="w-full">
+            <div className="bg-white p-4 rounded-lg shadow-md">
+              <div className="text-xl font-bold">Description</div>
+              <div className="mt-2">Julie</div>
+            </div>
+          </div>
+
+          <div className="w-full">
+            <div className="bg-white p-4 rounded-lg shadow-md flex items-center justify-center cursor-pointer" onClick={() => handleBook(post.owner)}>
+              <Primary text="BOOK NOW" />
+            </div>
+          </div>
+
+        </div>
+      ) : (
+        <div>Loading ...</div>
+      )}
+    </>
   );
 };
 
